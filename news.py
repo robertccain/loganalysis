8fc1b4cec9ff937b00f6cc49c943a74fd27db3cd
Robert C Cain <robert.c.cain@gmail.com>
2019-03-23 18:56:55 -0400

    Update news.py

--- news.py
+++ news.py
@@ -1,19 +1,38 @@
-#Python Project Logs Analysis
+# Python Project Logs Analysis
+# !/usr/bin/env python
+# for python2
+# Import psycopg2 tool
 import psycopg2
-
 DBNAME = "news"
-
-#connect  to db
+# connect  to db
 connect = psycopg2.connect(database=DBNAME)
 # Open a cursor to perform database operations
 cur = connect.cursor()
 # Execute a command: this creates a new table
-
-#question 1
-cur.execute("select * from log")
-
+# question 1
+cur.execute("select * from popauth;)"
+# 1. What are the most popular three articles of all time?
 # Query the database and obtain data as Python objects
-news_fetch = cur.fetchall()
-#output returns from Query on sepeate lines
-for new_entry in news_fetch:
-    print(new_entry)
+news_fetch_art = cur.fetchall()
+# Output returns from Query on sepeate lines
+for top_articles in news_fetch_art:
+    print(top_articles)
+#        --- Insert a blank line between each question ---
+# question 2
+cur.execute("select * from popart;)"
+# 2. Who are the most popular article authors of all time?
+# Query the database and obtain data as Python objects
+news_fetch_auth = cur.fetchall()
+# output returns from Query on sepeate lines
+for top_auth in news_fetch_auth:
+    print(top_auth)
+#           --- Insert a blank line between each question ---
+# Question 3
+# 3. On which days did more than 1% of requests lead to errors?
+cur.execute("select * from final_output;")
+# Query the database and obtain data as Python objects
+news_fetch_error = cur.fetchall()
+# Output returns from Query on sepeate lines
+for Perrors in news_fetch_error:
+    print(Perrors)
+#            --- Insert a blank line ---
