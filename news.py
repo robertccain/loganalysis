10521281bbb1f62741cf323247cce8228d40ebef
Robert C Cain <robert.c.cain@gmail.com>
2019-03-26 19:20:16 -0400

    Update news.py

--- news.py
+++ news.py
@@ -9,7 +9,7 @@ connect = psycopg2.connect(database=DBNAME)
 query_1 = connect.cursor()
 # Execute a command: this creates a new table
 # question 1
-query_1.execute("select * from popauth;")
+query_1.execute("select * from popart;")
 # 1. What are the most popular three articles of all time?
 # Query the database and obtain data as Python objects
 news_fetch = query_1.fetchall()
@@ -17,22 +17,24 @@ for topArticles in news_fetch:
     print(news_fetch)
 connect.close()
 # --- Insert a blank line between each question ---
-query_2 = connect.cursor()
-query_2.execute("select * from popart;")
+con = psycopg2.connect(database=DBNAME)
+query_2 = con.cursor()
+query_2.execute("select * from popauth;")
 # 2.Who are the most popular article authors of all time?
 # Query the database and obtain data as Python objects
 news_fetch_auth = query_2.fetchall()
 # output returns from Query on sepeate lines
 for TopAuthors in news_fetch_auth:
     print(news_fetch_auth)
-connect.close()
+con.close()
 # --- Insert a blank line between each question ---
-query_3 = connect.cursor()
-query_3.execute("select * from final_output;")
+c = psycopg2.connect(database=DBNAME)
+query_3 = c.cursor()
+query_3.execute("select * from final_output where test > 1.0;")
 # 3. On which days did more than 1% of requests lead to errors?
 # Query the database and obtain data as Python objects
 news_fetch_error = query_3.fetchall()
 # Output returns from Query on sepeate lines
 for Perrors in news_fetch_error:
     print(Perrors)
-connect.close()
+c.close()
