ca10953c0325426179a0e54dc553d864341414cb
Robert C Cain <robert.c.cain@gmail.com>
2019-03-31 13:46:28 -0400

    Update news.sql

--- news.sql
+++ news.sql
@@ -1,110 +1,41 @@
 --1. What are the most popular three articles of all time?--
 create view popart as
-SELECT log.path, COUNT(*) as num
-from log
-group by log.path
-HAVING log.path = '/article/bad-things-gone' or
-       log.path = '/article/balloon-goons-doomed' or
-       log.path = '/article/bears-love-berries' or
-       log.path = '/article/candidate-is-jerk' or
-       log.path = '/article/goats-eat-googles' or
-       log.path = '/article/media-obsessed-with-bears' or
-       log.path = '/article/trouble-for-troubled' or
-       log.path = '/article/so-many-bears'
-order by num desc
-limit 3;
+  SELECT title, count(*) as num FROM articles
+  INNER JOIN authors ON (authors.id = articles.author)
+  JOIN log ON (REPLACE(log.path, '/article/', '')=articles.slug)
+  group by title
+  order by num desc
+  limit 3;
 
 --2. Who are the most popular article authors of all time?
-
 create view popauth as
     SELECT name, count(*) as num FROM authors
     INNER JOIN articles ON (authors.id = articles.author)
     JOIN log ON (REPLACE(log.path, '/article/', '')=articles.slug)
     group by name
-    order by num desc
-    limit 3;
+    order by num desc;
 
 --3. On which days did more than 1% of requests lead to errors?
--- build errors
-
 create view dateco as
   select path, ip, method, status, id, cast(time as date) from log;
 
-create view statcounta as
-  select status, time, count(*) as total from dateco
-  group by status, time
-  order by time;
+create view stat_all as
+  select status, time as date, count(*) as total from dateco
+  group by status, date
+  order by date;
 --build ok
-create view statcountb as
-  select status as status_fail, time as time_fail, count(*) as fail from dateco
+create view stat_fail as
+  select status as status_fail, time as date_fail, count(*) as fail from dateco
   where status != '200 OK'
-  group by status_fail, time_fail
-  order by time_fail;
-
---build combo view
-create view statjoin as
-  select * from statcounta
-  join statcountb on (statcounta.time = statcountb.time_fail)
-  group by statcounta.status, statcounta.time,
-  statcounta.total, statcountb.status_fail, statcountb.time_fail,
-  statcountb.fail
-  order by statcountb.time_fail;
+  group by status_fail, date_fail
+  order by date_fail;
 
 -- final statement to create percentage calculation
 create view final_output as
-select status_fail, time_fail,
+select stat_fail.status_fail as status, stat_fail.date_fail as date,
   (select
-    (statjoin.fail::float*100)/statjoin.total::float as percentage) as test
-    from statjoin
-    group by status_fail, time_fail, test
+    (((stat_fail.fail::float)/stat_all.total::float)*100) as percentage) as test
+    from stat_fail
+    join stat_all on stat_all.date = stat_fail.date_fail
+    group by stat_fail.status_fail, stat_fail.date_fail, test
     order by test;
-
-
-
-"""
--- Every thing in this qoute block was used to during development and testing to build the statements above
-
-
--- for reference Testing tables--
--- created view to link authors to titles--
-create view countofrecords as
-  select articles.author, authors.name, articles.slug
-  from authors, articles
-  where authors.id = articles.author
-  group by authors.name, articles.slug, articles.author
-  order by articles.author;
-
-  --individual statements for testing
-
-      select log.path as name, count(*) as Num from log where path = '/article/bad-things-gone' group by name;   
-      -- 170098     
-      select log.path as name, count(*) as Num from log where path = '/article/balloon-goons-doomed' group by name;
-      -- 84557
-      select log.path as name, count(*) as Num from log where path = '/article/bears-love-berries' group by name;
-      -- 253801
-      select log.path as name, count(*) as Num from log where path = '/article/candidate-is-jerk' group by name;
-      -- 338647
-      select log.path as name, count(*) as Num from log where path = '/article/goats-eat-googles' group by name;
-      -- 84906
-      select log.path as name, count(*) as Num from log where path = '/article/media-obsessed-with-bears' group by name;
-      -- 84383
-      select log.path as name, count(*) as Num from log where path = '/article/trouble-for-troubled' group by name;
-      -- 84810
-      select log.path as name, count(*) as Num from log where path = '/article/so-many-bears' group by name;
-      -- 84504   
-
-
---gives top author by articles, test table
-create view popauth as
-      SELECT name, log.path, count(*) as num FROM authors
-      INNER JOIN articles ON (authors.id = articles.author)
-      JOIN log ON (REPLACE(log.path, '/article/', '')=articles.slug)
-      group by log.path, name
-      order by num desc;
-
--- used to view log tables status--
-select status, time from log
-where status <> '200 OK'
-group by status, time;
-
-"""
